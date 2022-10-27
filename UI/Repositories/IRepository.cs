730133440f35abe619218608d7fa1a8e454950d7
Agustin Barberis <agus_101@live.com.ar>
2022-10-26 23:06:57 -0300

    Creates repository interface.

---
+++ UI/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.Repositories
+{
+    public interface IRepository<TType, TKey> where TType : class
+    {
+        Task<TType> GetById(TKey id);
+        Task<IEnumerable<TType>> GetAll();
+        
+        Task<bool> Insert(TType entity);
+        Task<bool> Remove(TKey id);
+        Task<bool> Update(TType entity);
+        Task Save();
+
+    }
+}
