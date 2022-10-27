cee6bcf7a7fd53076d7f74648ac31ee968a68c05
Agustin Barberis <agus_101@live.com.ar>
2022-10-27 01:31:52 -0300

    Added isValidInt method

--- UI/InputValidator.cs
+++ UI/InputValidator.cs
@@ -2,6 +2,20 @@
 {
     internal class InputValidator
     {
+        internal static bool IsValidInt(string? input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return false;
+
+            foreach (char c in input)
+            {
+                if (!char.IsDigit(c))
+                    return false;
+            }
+
+            return true;
+        }
+
         internal static bool IsValidString(string? input)
         {
             if (string.IsNullOrWhiteSpace(input))
